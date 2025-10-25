2c82b5b1e99028742cbff48c32f8951c60e74713
Hans <sivhans3@gmail.com>
2025-10-25 19:25:33 +0200

    Learning variables and type casting in python

---
+++ Coding/Python/Basics/variables.py
@@ -0,0 +1,40 @@
+#Variables 
+#Strings
+
+first_name = 'John Smith'
+food = "lasagna"
+email = "jsmith336@yahoo.com"
+print(first_name)
+
+
+#F-String (display variable and string)
+print(f"Hello {first_name}")
+print(f"You like {food}")
+print(f"Your email is: {email} \n")
+
+#Integers
+age = 44
+quantity = 3
+
+print(f"You are {age} years old")
+print(f"You are buying {quantity} items \n")
+
+#Floats
+price = 10.99
+print(f"The total is ${price}\n")
+
+
+#Boolean
+is_dude = True #True and False are case-sensitve: Needs Capital Letters
+
+#If Statements
+if not(is_dude):
+    print("You're no dude brah \n")
+else:
+    print("Shah dude. Sweet \n")
+
+#Bro Code Assignment:
+user_name = "JSmithJr"
+year = 2025
+pi = 3.14
+is_admin = True
---
+++ Coding/Python/typecasting.py
@@ -0,0 +1,25 @@
+#Tutorial on typecasting
+
+name = "John Smith"
+age = 44
+gpa = 3.3
+is_student = True
+
+gpa = int(gpa)
+print("\n")
+print(gpa)
+
+print("\n")
+
+age = float(age)
+print(age)
+print("\n")
+
+age = str(age)
+age += "1"
+
+print(age)
+print("\n")
+
+name = bool(name)
+print(name)
