4073da42ae0997aecd10ea8d4addec04a56db359
Hans <sivhans3@gmail.com>
2026-01-27 20:36:40 +0200

    Learning(Java) : Changed Server.java file - Added annotations

--- Coding/Java/Basics/5-Advanced_OOP/src/oop/advanced/Server.java
+++ Coding/Java/Basics/5-Advanced_OOP/src/oop/advanced/Server.java
@@ -7,38 +7,47 @@ public abstract class Server implements Computer{
     String[] components;
 
     //Getters & Setters
+    @Override
     public String getProcessor() {
         return mainProcessor;
     }
 
+    @Override
     public void setProcessor(String processor) {
         this.mainProcessor = processor;
     }
 
+    @Override
     public int getNumCores() {
         return numCores;
     }
 
+    @Override
     public void setNumCores(int numCores) {
         this.numCores = numCores;
     }
 
+    @Override
     public double getProcessingPower() {
         return processingPower;
     }
 
+    @Override
     public void setProcessingPower(double processingPower) {
         this.processingPower = processingPower;
     }
 
+    @Override
     public String[] getComponents() {
         return components;
     }
 
+    @Override
     public void setComponents(String[] components) {
         this.components = components;
     }
 
+    @Override
     //Methods
     public void bootUp(){
         System.out.println("System has booted up. Device is now on");
