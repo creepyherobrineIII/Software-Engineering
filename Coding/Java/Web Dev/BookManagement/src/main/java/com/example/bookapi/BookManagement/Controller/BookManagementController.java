a245e0f8d1d619c1129b717e8cb972afc6f31eac
Hans <sivhans3@gmail.com>
2026-03-01 17:59:49 +0200

    Learning(Spring Boot) - Creating Book Management API

---
+++ Coding/Java/Web Dev/BookManagement/src/main/java/com/example/bookapi/BookManagement/Controller/BookManagementController.java
@@ -0,0 +1,20 @@
+package com.example.bookapi.BookManagement.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/bookApi/books")
+class BookManagementController {
+    @GetMapping("/")
+    void getAllBooks(){
+        
+    }
+
+    @GetMapping("/{@requestedId}")
+    void getBookById(@PathVariable Long requestedId){
+
+    }
+}
